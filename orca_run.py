64e02667e88fadacedeb5ae08997400a05218db9
chinmayhd <Tushar_Chinmaydeshmukh.hgwgst@n.w.rd.honda.co.jp>
2023-09-01 18:04:04 +0900

    plot velocities instead of positions

--- orca_run.py
+++ orca_run.py
@@ -5,9 +5,9 @@ from pyorca import Agent, orca
 
 # Create a list of agents (robots)
 agents = [
-    Agent(position=[-4, 4], velocity=[-0.4, 0.4], radius=0.1, max_speed=1.0, pref_velocity=[1, 0]),
-    Agent(position=[0, 0], velocity=[-0.4, 0.4], radius=0.1, max_speed=1.0, pref_velocity=[-1, 0]),
-    Agent(position=[2, 2], velocity=[0.5, -0.5], radius=0.1, max_speed=1.0, pref_velocity=[0.5, 0]),
+    Agent(position=[-2, 2], velocity=[0.1, 0.1], radius=0.1, max_speed=1.0, pref_velocity=[-0.1, 0.1]),
+    Agent(position=[0, 0], velocity=[-0.2, -0.2], radius=0.1, max_speed=1.0, pref_velocity=[0.1, 0.1]),
+    # Agent(position=[2, 2], velocity=[-0.5, -0.5], radius=0.1, max_speed=1.0, pref_velocity=[0.5, 0]),
     # Add more agents as needed
 ]
 
@@ -18,8 +18,8 @@ cmap = get_cmap('tab10')
 plt.figure(figsize=(6, 6))
 plt.xlim(-10, 10)
 plt.ylim(-10, 10)
-plt.xlabel('X')
-plt.ylabel('Y')
+plt.xlabel('Velocity Y')
+plt.ylabel('Velocity X')
 plt.grid(True)
 
 # Turn on interactive mode
@@ -28,42 +28,39 @@ plt.ion()
 # Simulation parameters
 time_step = 0.1
 total_time = 10.0
-cnt = 0
+agent_num = 0
 
 # Simulate the scenario
 for t in np.arange(0.1, total_time, time_step):
     plt.clf()  # Clear the previous plot
 
     for agent in agents:
+        for idx in range(len(agents)):
+            if np.array_equal(agent.position, agents[idx].position):
+                agent_num = idx
+
         # Compute ORCA solution for the agent
         colliding_agents = [other_agent for other_agent in agents if other_agent != agent]
         new_velocity, half_planes = orca(agent, colliding_agents, t, time_step)
+        print(f"New velocity is {new_velocity} for agent {agent_num}")
 
         # Update agent's velocity
         agent.velocity = new_velocity
 
-        # Update agent's position using np.add
-        np.add(agent.position, agent.velocity * time_step, out=agent.position, casting="unsafe")
-
-        # Plot the agent's position with color from colormap
+        # Plot the agent's velocity with color from colormap
         color = cmap(agents.index(agent))
-        plt.plot(agent.position[0], agent.position[1], 'o', markersize=8, label=f'Robot {agents.index(agent)}',
-                 color=color)
 
-        # Plot the avoidance velocity vector as an arrow
-        plt.arrow(agent.position[0], agent.position[1], agent.velocity[0], agent.velocity[1],
-                  color=color, alpha=0.5, head_width=0.1, head_length=0.2)
+        plt.scatter(agent.velocity[1], agent.velocity[0], label='Agent', marker='o')
 
-        # Plot the half-planes with different color
-        for i, line in enumerate(half_planes):
-            anchor = line.point
-            direction = line.direction
-            plt.plot([anchor[0], anchor[0] + direction[0]],
-                     [anchor[1], anchor[1] + direction[1]], '--', label=f'Half-Plane {cnt}', color=color)
-            cnt += 1
-            if cnt == len(agents):
-                cnt = 0
+        # Plot the half-planes
+        for line in half_planes:
+            # Plot the half-plane as a line
+            plt.plot([line.point[1], line.point[1] + line.direction[1]],
+                     [line.point[0], line.point[0] + line.direction[0]], label=f'Half-Plane {agent_num}')
 
+        # Plot the avoidance velocities
+        plt.arrow(agent.velocity[1], agent.velocity[0], agent.velocity[1], agent.velocity[0], head_width=0.05, head_length=0.1,
+                  label='Avoidance Velocity', color=color)
 
     plt.title(f'Time: {t:.1f} seconds')
     plt.legend()  # Add legend
@@ -73,5 +70,6 @@ for t in np.arange(0.1, total_time, time_step):
 plt.ioff()
 
 plt.show()  # Display the final plot
+
 
 
